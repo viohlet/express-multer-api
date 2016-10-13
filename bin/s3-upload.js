ddcd023052b8aea19bee233751ba47ec902daf43
viohlet <violetavenegas@hotmail.com>
2016-10-13 15:04:44 -0400

    Determine file/mime-type
    Use the file-type module to determine the content type of the upload
    (mime-type)
    Define a function that gives us a default file-type and mime-type
    in the event that the file-type module is given an unsupported file.

--- bin/s3-upload.js
+++ bin/s3-upload.js
@@ -2,6 +2,7 @@
 'use strict';
 
 const fs = require('fs'); //fs and no .fs because it is built in into mode
+const fileType = require('file-type');
 
 const filename = process.argv[2] || ''; //how we get things out of the command line. Access 3rd arg
 
@@ -16,12 +17,28 @@ const readFile = (filename) => {
   });
 };
 
-const logMessage = (data) => {
-  console.log(`${filename} is ${data.length} bytes long`); // (`) starts a "template literal" (substitute the value of whatever is going through). string interpolation.
+//return a default object in the case that fileType is given an unsupported
+//filetype to read
+const mimeType = (data) => {
+    return Object.assign({
+      ext: 'bin',
+      mime: 'application/octet-stream', //just a string of bites, binary file just got info in it
+    }, fileType(data));
+};
+
+const parseFile = (fileBuffer) => {
+  let file = mimeType(fileBuffer);
+  file.data = fileBuffer;
+  return file;
+};
+
+const logMessage = (file) => {
+  console.log(`${filename} is ${file.data.length} bytes long and is of mime ${file.mime}`); // (`) starts a "template literal" (substitute the value of whatever is going through). string interpolation.
   //run the script here to see if it works. go to package.json and add ""s3-upload": "./bin/s3-upload.js". Then npm s3-upload and chmod +x... until we get error enoent
 };
 
 readFile(filename)
+.then(parseFile)
 .then(logMessage)
 .catch(console.error)
 ;
