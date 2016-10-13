64b0438d51446d672fa53e922ba46f2e2fd7d9f1
viohlet <violetavenegas@hotmail.com>
2016-10-13 13:58:45 -0400

    Wrap fs.readFile in a Promise.
    Define a wrapper function for fs.readFile to aid readability. Also
    define a wrapper for the logger we will change later to do something
    useful (like)...

--- bin/s3-upload.js
+++ bin/s3-upload.js
@@ -6,11 +6,23 @@ const fs = require('fs'); //fs and no .fs because it is built in into mode
 
 const filename = process.argv[2] || ''; //how we get things out of the command line. Access 3rd arg
 
-fs.readFile(filename, (error, data) => { //readFile passes error or data not both
-  if (error) {
-    return console.error(error); //return a real error later. so the file exists
-  }
+const readFile = (filename) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filename, (error, data) => { //readFile passes error or data not both
+      if (error) {
+        reject(error);
+      }
+      resolve(data);
+    });
+  });
+};
 
+const logMessage = (data) => {
   console.log(`${filename} is ${data.length} bytes long`); // (`) starts a "template literal" (substitute the value of whatever is going through). string interpolation.
   //run the script here to see if it works. go to package.json and add ""s3-upload": "./bin/s3-upload.js". Then npm s3-upload and chmod +x... until we get error enoent
-});
+};
+
+readFile(filename)
+.then(logMessage)
+.catch(console.error)
+;
